cb41bfaceb19986ca5b044ae6b67debbe281f454
saiharshav <vsharsha48@gmail.com>
2024-06-30 22:05:23 +0530

    add buttons and beautify

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get('/api/projects', (req, res) => {
 
 app.get('/api/skills', (req, res) => {
     const skills = [
-        {id: 1, title: 'C++', image: "https://w7.pngwing.com/pngs/579/803/png-transparent-the-c-programming-language-programmer-computer-programming-programming-blue-logo-computer-program-thumbnail.png", description: 'Proficient in C++ programming and problem solving.'},
-        {id: 2, title: 'Python', image: 'https://banner2.cleanpng.com/20180402/ioq/kisspng-python-logo-clojure-javascript-9-5ac25c2686ca38.9179638515226870145521.jpg', description: 'Developed numorous automation scripts using Python programming language.'},
-        {id: 3, title: 'Javascript', image: "https://banner2.cleanpng.com/20180810/biz/kisspng-javascript-scalable-vector-graphics-logo-encapsula-javascript-le-ekran-grnts-almak-alpere-5b6dbeb48e4583.2854840415339189005828.jpg", description: 'Developed multiple web applications using Javascript programming language. Please have a look at them in the projects section.'}, 
-        {id: 4, title: 'Nodejs', image: "https://banner2.cleanpng.com/20180421/feq/kisspng-web-development-node-js-socket-io-javascript-netwo-modernization-5adab811677023.4675225415242834094237.jpg", description: 'Created server-side applications and APIs using Nodejs. Please have a look at them in the projects section.'},
+        {id: 1, title: 'C++', image: `https://cdn.icon-icons.com/icons2/3398/PNG/512/plus_logo_c_icon_214621.png`, description: 'Proficient in C++ programming, problem solving, and Algorithms.'},
+        {id: 2, title: 'Python', image: 'https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/Python_logo-512.png', description: 'Developed numorous automation scripts using Python programming language.'},
+        {id: 3, title: 'Javascript', image: "https://w7.pngwing.com/pngs/559/894/png-transparent-js-square-brands-icon.png", description: 'Developed multiple web applications using Javascript programming language. Please have a look at them in the projects section.'}, 
+        {id: 4, title: 'Nodejs', image: "https://cdn.icon-icons.com/icons2/2622/PNG/512/brand_nodejs_icon_157858.png", description: 'Created server-side applications and APIs using Nodejs. Please have a look at them in the projects section.'},
     ]
     res.json(skills);
 })
