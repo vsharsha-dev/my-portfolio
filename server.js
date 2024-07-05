c1b499fead0742279782276952bccc6b2488534f
saiharshav <vsharsha48@gmail.com>
2024-07-05 23:45:23 +0530

    Add projects

--- server.js
+++ server.js
@@ -22,10 +22,15 @@ app.get('/api/projects', (req, res) => {
     const projects = [
         {id: 1, title: 'AnimeBio', image: '/static/images/portfolio-animebio.png', description: 'Developed a website to provide users informtion about Anime searched by them. It gives details such as the plot, description, rating, etc.'},
         {id: 2, title: 'To Do List', image: '/static/images/portfolio-todolist.png', description: 'A ToDoList web application to add or remove items from the list. Connected it to a database to use dynamically.'},
-        {id: 3, title: 'COVID19 Tracker', image: '/static/images/portfolio-covidapp.png', description: 'Developed and deployed a web application to give statistics of current COVID-19 scenario.'},
-        {id: 4, title: 'Tindog', image: '/static/images/portfolio-tindog.png', description: 'Designed a Tinder clone app using CSS.'},
-        {id: 5, title: 'FB Login Page', image: '/static/images/portfolio-fblogin.png', description: 'Replicated the Facebook desktop login page from scratch by using conventional HTML and CSS.'},
-        {id: 6, title: 'Simon Game', image: '/static/images/portfolio-simongame.png', description: 'Created a simple interactive game using CSS and JavaScript.'},
+        {id: 3, title: 'Travel Planner', image: '/static/images/portfolio-placepicker.jpeg', description: 'Developed an app to select places to travel. Integrated backend to frontend for seamless data exchange. Created React custom Hooks to call backend API endpoints.'},
+        {id: 4, title: 'COVID19 Tracker', image: '/static/images/portfolio-covidapp.png', description: 'Developed and deployed a web application to give statistics of current COVID-19 scenario.'},
+        {id: 5, title: 'Countdown Game', image: '/static/images/portfolio-timegame.jpeg', description: 'Developed a React application to play a time countdown game where user can test their accuracy of time sense.'},
+        {id: 6, title: 'Quiz App', image: '/static/images/portfolio-quizapp.jpeg', description: 'Created a React quiz application which scores the user based on their answers. Implemnted timer for each question.'},
+        {id: 7, title: 'SIP Calculator', image: '/static/images/portfolio-investmentcalculator.jpeg', description: 'A simple react application which can be used to calculate returns on an investment with the given rate of interest.'},
+        {id: 8, title: 'Project Management', image: '/static/images/portfolio-projectmanagement.png', description: 'React app which tracks and displays the projects which need to be done similar to a TODO list.'},
+        {id: 9, title: 'Tindog', image: '/static/images/portfolio-tindog.png', description: 'Designed a Tinder clone app using CSS.'},
+        {id: 10, title: 'FB Login Page', image: '/static/images/portfolio-fblogin.png', description: 'Replicated the Facebook desktop login page from scratch by using conventional HTML and CSS.'},
+        {id: 11, title: 'Simon Game', image: '/static/images/portfolio-simongame.png', description: 'Created a simple interactive game using CSS and JavaScript.'},
     ]
     res.json(projects);
 })
