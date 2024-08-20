afef7e62878bb473a455487845cfee93861f404b
saiharshav <vsharsha48@gmail.com>
2024-08-20 12:16:27 +0530

    add build folder to git

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { PROJECTS } from "./db/projects.js";
 import { BLOGS } from "./db/blogs.js";
 import { SKILLS } from "./db/skills.js";
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Get directory name
@@ -18,24 +18,26 @@ const __dirname = dirname(__filename);
 app.use(cors());
 
 // serve static files from the react app
-app.use('/static', express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 
-// 
-app.get('/api/projects', (req, res) => {
-    res.json(PROJECTS);
-})
+//
+app.get("/api/projects", (req, res) => {
+  res.json(PROJECTS);
+});
 
-app.get('/api/skills', (req, res) => {
-    res.json(SKILLS);
-})
+app.get("/api/skills", (req, res) => {
+  res.json(SKILLS);
+});
 
-app.get('/api/blogs', (req, res) => {
-    res.json(BLOGS);
-})
+app.get("/api/blogs", (req, res) => {
+  res.json(BLOGS);
+});
 
 // All other requests will be handled by the React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
-app.listen(PORT, () => {console.log(`Server is running at port ${PORT}`);})
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
